cc68b6cba1730d4439b2e87dd199e6de76d600e8
irishk07 <prygunova.irina07@mail.ru>
2025-10-07 12:31:52 +0300

    общий хэдер с командами

---
+++ common.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+
+enum code_comand {
+    CMD_PUSH  = 1,
+    CMD_POP   = 2,
+    CMD_ADD   = 3,
+    CMD_SUB   = 4,
+    CMD_DIV   = 5,
+    CMD_MUL   = 6,
+    CMD_SQRT  = 7,
+    CMD_POW   = 8,
+    CMD_IN    = 9,
+    CMD_OUT   = 10,
+    CMD_HLT   = 11,
+    CMD_JMP   = 12,
+    CMD_POPR  = 42,
+    CMD_PUSHR = 33
+};
+
+#endif // COMMON_H_
