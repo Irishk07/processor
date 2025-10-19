ddb350c65247ba0215e09d46d9cc26aaa7c98812
irishk07 <prygunova.irina07@mail.ru>
2025-10-19 15:38:26 +0300

    listing_file, current_command

--- asm/asm.cpp
+++ asm/asm.cpp
@@ -10,40 +10,6 @@
 #include "onegin.h"
 
 
-void AsmInitInfoAboutCommands(Assembler* assembler) {
-    assert(assembler);
-    
-    for (int i = 0; i < MAX_CNT_COMMANDS; ++i) {
-        assembler->about_commands[i] = {};
-    }
-
-    // FIXME
-    assembler->about_commands[CMD_PUSH]  = {.command_name = "PUSH",  .command_code = CMD_PUSH,  .code_of_type_argument = NUM_ARGUMENT};
-    assembler->about_commands[CMD_POP]   = {.command_name = "POP",   .command_code = CMD_POP,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_ADD]   = {.command_name = "ADD",   .command_code = CMD_ADD,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_SUB]   = {.command_name = "SUB",   .command_code = CMD_SUB,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_DIV]   = {.command_name = "DIV",   .command_code = CMD_DIV,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_MUL]   = {.command_name = "MUL",   .command_code = CMD_MUL,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_SQRT]  = {.command_name = "SQRT",  .command_code = CMD_SQRT,  .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_POW]   = {.command_name = "POW",   .command_code = CMD_POW,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_IN]    = {.command_name = "IN",    .command_code = CMD_IN,    .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_OUT]   = {.command_name = "OUT",   .command_code = CMD_OUT,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_HLT]   = {.command_name = "HLT",   .command_code = CMD_HLT,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_JMP]   = {.command_name = "JMP",   .command_code = CMD_JMP,   .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JB]    = {.command_name = "JB",    .command_code = CMD_JB,    .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JBE]   = {.command_name = "JBE",   .command_code = CMD_JBE,   .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JA]    = {.command_name = "JA",    .command_code = CMD_JA,    .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JAE]   = {.command_name = "JAE",   .command_code = CMD_JAE,   .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JE]    = {.command_name = "JE",    .command_code = CMD_JE,    .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_JNE]   = {.command_name = "JNE",   .command_code = CMD_JNE,   .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_CALL]  = {.command_name = "CALL",  .command_code = CMD_CALL,  .code_of_type_argument = LABEL_ARGUMENT};
-    assembler->about_commands[CMD_RET]   = {.command_name = "RET",   .command_code = CMD_RET,   .code_of_type_argument = NO_ARGUMENT};
-    assembler->about_commands[CMD_PUSHR] = {.command_name = "PUSHR", .command_code = CMD_PUSHR, .code_of_type_argument = REG_ARGUMENT};
-    assembler->about_commands[CMD_POPR]  = {.command_name = "POPR",  .command_code = CMD_POPR,  .code_of_type_argument = REG_ARGUMENT};
-    assembler->about_commands[CMD_PUSHM] = {.command_name = "PUSHM", .command_code = CMD_PUSHM, .code_of_type_argument = RAM_REG_ARGUMENT};
-    assembler->about_commands[CMD_POPM]  = {.command_name = "POPM",  .command_code = CMD_POPM,  .code_of_type_argument = RAM_REG_ARGUMENT};
-}
-
 void AsmInitLabels(Assembler* assembler) {
     for (int i = 0; i < CNT_LABELS; ++i) {
         assembler->labels[i] = -1;
@@ -55,9 +21,7 @@ assembler_status AsmCtor(Assembler* assembler, const char* name_commands_file) {
     assert(name_commands_file);
 
     assembler->about_text.text_name = name_commands_file;
-    assembler->cnt_commands            = 0;
-
-    AsmInitInfoAboutCommands(assembler);
+    assembler->byte_code_data.size  = 0;
 
     AsmInitLabels(assembler);
 
@@ -87,41 +51,45 @@ assembler_status AsmVerify(const Assembler* assembler, int number_of_compile) {
     return ASM_SUCCESS;
 }
 
-
-status_cmp FillCommand(Assembler* assembler, char* command, int index, int number_of_compile) { //FIXME Verify how realize
+status_cmp FindCommand(Assembler* assembler, char* string, About_commands* current_command) {
     assert(assembler);
+    assert(string);
+    assert(current_command);
 
-    if (assembler->about_commands[index].command_name == NULL) {
-        return DIFFERENT;
-    }
-
-    if(!strcmp(assembler->about_commands[index].command_name, (const char*)command)) {
-        if (number_of_compile == FIRST_COMPILE) {
-            assembler->cnt_commands++;
+    for(int i = 0; i < MAX_CNT_COMMANDS; ++i) {
+        if(!strcmp(about_commands[i].command_name, (const char*)string)) {
+            *current_command = about_commands[i];
             return EQUAL;
-        } 
-
-        assembler->byte_code_data.data[assembler->byte_code_data.size] = assembler->about_commands[index].command_code;
-        assembler->byte_code_data.size++;
-
-        return EQUAL;
+        }
     }
 
     return DIFFERENT;
 }
 
-assembler_status GetFillArgNum(Assembler* assembler, char* string, int number_of_compile) {
+void FillCommand(Assembler* assembler, About_commands* current_command, int number_of_compile) {
+    assert(assembler);
+    assert(current_command);
+
+    if (number_of_compile == SECOND_COMPILE) {
+        assembler->byte_code_data.data[assembler->byte_code_data.size] = current_command->command_code;
+    }
+
+    assembler->byte_code_data.size++;
+}
+
+assembler_status GetFillArgNum(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile) {
+    assert(string);
+
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
 
     type_t number = 0;
 
     if (sscanf(string, TYPE_T_PRINTF_SPECIFIER, &number) == 1) {
-        if (number_of_compile == FIRST_COMPILE) {
-            assembler->cnt_commands++;
-            return ASM_SUCCESS;
-        } 
+        if (number_of_compile == SECOND_COMPILE) {
+            assembler->byte_code_data.data[assembler->byte_code_data.size] = number;
+            current_command->argument = number;
+        }
 
-        assembler->byte_code_data.data[assembler->byte_code_data.size] = number;
         assembler->byte_code_data.size++;
     }
     else {
@@ -134,39 +102,39 @@ assembler_status GetFillArgNum(Assembler* assembler, char* string, int number_of
 }
 
 status_cmp CheckRegister(Assembler* assembler, char* string, int type_argument) {
-    if (type_argument == REG_ARGUMENT) {
-        for (int i = 0; i < CNT_REGISTERS; ++i) {
-            if (!strcmp(assembler->name_registers[i], string)) return EQUAL;
-        }
-    }
+    assert(string);
 
-    if (type_argument == RAM_REG_ARGUMENT) {
-        for (int i = 0; i < CNT_REGISTERS; ++i) { // +2 because argument has [ ]
-            if (!strcmp(assembler->name_ram_registers[i], string)) return EQUAL;
+    for (int i = 0; i < CNT_REGISTERS; ++i) {
+        if (!strncmp(assembler->name_registers[i], string, LEN_NAME_REGISTER)) {
+            if (type_argument == REG_ARGUMENT && strlen(string) == LEN_NAME_REGISTER)
+                return EQUAL;
+
+            if (type_argument == RAM_REG_ARGUMENT && strlen(string) == LEN_NAME_REGISTER + 1 //+1 because nam has ']' at the end
+                                                  && string[LEN_NAME_REGISTER] == ']') 
+                return EQUAL;
         }
     }
 
     return DIFFERENT;
 }
 
-assembler_status GetFillArgReg(Assembler* assembler, char* string, int number_of_compile, int type_argument) {
+assembler_status GetFillArgReg(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile, int type_argument) {
+    assert(string);
+
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
+
+    if (string[0] == '[') string++;
 
     if (!CheckRegister(assembler, string, type_argument)) {
         return ASM_EXPECTS_REGISTER;
     }
 
-    if (number_of_compile == FIRST_COMPILE) {
-        assembler->cnt_commands++;
-        return ASM_SUCCESS;
+    if (number_of_compile == SECOND_COMPILE) {
+        type_t code_reg = string[1] - 'A';
+        assembler->byte_code_data.data[assembler->byte_code_data.size] = code_reg;
+        current_command->argument = code_reg;
     }
 
-    type_t code_reg = 0;
-    // if argument is reg, we are get first index (middle element)
-    // if argument is arm_reg, we are get second index (middle element, we also have [ ])
-    (type_argument == REG_ARGUMENT) ? code_reg = string[1] - 'A' : code_reg = string[2] - 'A';
-
-    assembler->byte_code_data.data[assembler->byte_code_data.size] = code_reg;
     assembler->byte_code_data.size++;
 
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
@@ -174,51 +142,86 @@ assembler_status GetFillArgReg(Assembler* assembler, char* string, int number_of
     return ASM_SUCCESS;
 }
 
-assembler_status GetFillArgJump(Assembler* assembler, char* string, int number_of_compile) {
-    if (GetFillArgNum(assembler, string, number_of_compile) == ASM_SUCCESS) {
+assembler_status GetFillArgJump(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile) {
+    assert(string);
+
+    if (GetFillArgNum(assembler, current_command, string, number_of_compile) == ASM_SUCCESS) {
         return ASM_SUCCESS;
     }
 
-    if (*string == ':') {                                         
-        int number = 0;                                                         
-                                                                                
-        if (sscanf(string, "%*c%d", &number) == 1) {
-            if (0 <= number && number <= 9) {
-                if (number_of_compile == FIRST_COMPILE) {
-                    assembler->cnt_commands++;
-                    return ASM_SUCCESS;
-                } 
-
-                if (number_of_compile == SECOND_COMPILE && assembler->labels[number] == -1) {
-                    return ASM_NOT_FOUND_LABEL;
-                }
-
-                if (number_of_compile == SECOND_COMPILE) {
-                    assembler->byte_code_data.data[assembler->byte_code_data.size] = assembler->labels[number];
-                    assembler->byte_code_data.size++;
-                }
-
-                return ASM_SUCCESS;
-            }
-
-            return ASM_EXPECTS_JUMP_ARG;
-        }
+    if (*string != ':') {   
+        return ASM_EXPECTS_JUMP_ARG;
     }
-    
+
+    int number = 0;                                                                                                       
+    if (sscanf(string, "%*c%d", &number) != 1) {
+        return ASM_EXPECTS_JUMP_ARG;
+    }
+
+    if (0 <= number && number <= 9) {
+        if (number_of_compile == SECOND_COMPILE) {
+            if (assembler->labels[number] == -1) return ASM_NOT_FOUND_LABEL;
+
+            assembler->byte_code_data.data[assembler->byte_code_data.size] = assembler->labels[number];
+            current_command->argument = assembler->labels[number];
+        }
+
+        assembler->byte_code_data.size++;
+
+        return ASM_SUCCESS;
+    }
+
     return ASM_EXPECTS_JUMP_ARG;
 }
 
+assembler_status CheckLabel(Assembler* assembler, char* string) {
+    assert(string);
+
+    if (*string == ':') {                                                       
+        int number = 0;                                                         
+                                                                                
+        if (sscanf(string, "%*c%d", &number) != 1) {                            
+            return ASM_NOT_FOUND_LABEL;                                          
+        }       
+        
+        assembler->labels[number] = (type_t)assembler->byte_code_data.size;     
+        
+        return ASM_SUCCESS;
+    }
+
+   return ASM_NOT_FOUND_LABEL;
+}
+
+void FillListingFile(char* pointer_on_command, About_commands* current_command, FILE* listing_file, int type_argument) {
+    assert(pointer_on_command);
+    assert(current_command);
+    assert(listing_file);
+
+    fprintf(listing_file, "%p ", pointer_on_command);
+    fprintf(listing_file, "%-8s ", current_command->command_name);
+    fprintf(listing_file, "%-5ld ", current_command->command_code);
+
+    if (type_argument != NO_ARGUMENT) {
+        fprintf(listing_file, TYPE_T_PRINTF_SPECIFIER, current_command->argument);
+    }
+
+    fprintf(listing_file, "\n");
+}
 
 assembler_status PrintfByteCode(Assembler* assembler, int number_of_compile) {
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
 
     fprintf(stderr, "Compile number %d:\n", number_of_compile);
 
-    fprintf(stderr, "Byte code:\n");
-    for (size_t i = 0; i < assembler->byte_code_data.size; ++i) {
-        fprintf(stderr, TYPE_T_PRINTF_SPECIFIER " ", assembler->byte_code_data.data[i]);
+    if (number_of_compile == SECOND_COMPILE) {
+        fprintf(stderr, "Byte code:\n");
+
+        for (size_t i = 0; i < assembler->byte_code_data.size; ++i) {
+            fprintf(stderr, TYPE_T_PRINTF_SPECIFIER " ", assembler->byte_code_data.data[i]);
+        }
+
+        fprintf(stderr, "\n");
     }
-    fprintf(stderr, "\n");
 
     fprintf(stderr, "Labels:\n");
     for (int i = 0; i < CNT_LABELS; ++i) {
@@ -231,46 +234,73 @@ assembler_status PrintfByteCode(Assembler* assembler, int number_of_compile) {
     // getchar(); // TODO print message
 }
 
+#define OPEN_LISTING_FILE(file)                                  \
+        if (number_of_compile == SECOND_COMPILE) {               \
+            file = fopen("listing_file.txt", "w");               \
+                                                                 \
+            if (file == NULL) {                                  \
+                CHECK_AND_RETURN_ERRORS_ASM(ASM_OPEN_ERROR);     \
+            }                                                    \
+        }
+
+#define CLOSE_LISTING_FILE(file)                                                     \
+        if (number_of_compile == SECOND_COMPILE) {                                    \
+            if (fclose(file) == EOF) {                                        \
+                CHECK_AND_RETURN_ERRORS_ASM(ASM_CLOSE_ERROR,    perror("Error is:")); \
+            }                                                                         \
+        }
+
 assembler_status Assemblirovanie(Assembler* assembler, int number_of_compile) {
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
 
-    for (int i = 0; i < assembler->about_text.cnt_strok; ++i) {
-        int index = 0;
+    FILE* listing_file = NULL;
+    OPEN_LISTING_FILE(listing_file);
 
-        if (!strcmp(assembler->about_text.pointer_on_text[i], "\0")) {
+    bool find_cmd_hlt = false;
+
+    for (int i = 0; i < assembler->about_text.cnt_strok; ++i) {
+        CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
+
+        if (strlen(assembler->about_text.pointer_on_text[i]) == 0) {
             continue;
         }
 
-        for( ; index < MAX_CNT_COMMANDS; ++index) {
-            CHECK_LABEL(assembler->about_text.pointer_on_text[i]);
-            
-            if (FillCommand(assembler, assembler->about_text.pointer_on_text[i], index, number_of_compile)) {
+        if (CheckLabel(assembler, assembler->about_text.pointer_on_text[i]) == ASM_SUCCESS) continue;
 
-                if (assembler->about_commands[index].code_of_type_argument == NUM_ARGUMENT)   
-                    CHECK_AND_RETURN_ERRORS_ASM(GetFillArgNum(assembler, assembler->about_text.pointer_on_text[++i], number_of_compile));
-                    
-                if (assembler->about_commands[index].code_of_type_argument == REG_ARGUMENT ||
-                    assembler->about_commands[index].code_of_type_argument == RAM_REG_ARGUMENT)   
-                    CHECK_AND_RETURN_ERRORS_ASM(GetFillArgReg(assembler, assembler->about_text.pointer_on_text[++i], 
-                                                              number_of_compile, assembler->about_commands[index].code_of_type_argument));
-
-                if (assembler->about_commands[index].code_of_type_argument == LABEL_ARGUMENT)
-                    CHECK_AND_RETURN_ERRORS_ASM(GetFillArgJump(assembler, assembler->about_text.pointer_on_text[++i], number_of_compile));
-                
-                break;
-            }
-        }
-
-        if (index == MAX_CNT_COMMANDS) {
+        About_commands current_command = {};
+        if (!FindCommand(assembler, assembler->about_text.pointer_on_text[i], &current_command)) {
             CHECK_AND_RETURN_ERRORS_ASM(ASM_UNKNOWN_COMAND);
         }
+        
+        FillCommand(assembler, &current_command, number_of_compile);
+
+        if (current_command.command_code == CMD_HLT) find_cmd_hlt = true;
+
+        if (current_command.code_of_type_argument == NUM_ARGUMENT)   
+            CHECK_AND_RETURN_ERRORS_ASM(GetFillArgNum(assembler, &current_command, assembler->about_text.pointer_on_text[++i], number_of_compile));
+            
+        if (current_command.code_of_type_argument == REG_ARGUMENT ||
+            current_command.code_of_type_argument == RAM_REG_ARGUMENT)   
+            CHECK_AND_RETURN_ERRORS_ASM(GetFillArgReg(assembler, &current_command, assembler->about_text.pointer_on_text[++i], 
+                                                        number_of_compile, current_command.code_of_type_argument));
+
+        if (current_command.code_of_type_argument == LABEL_ARGUMENT)
+            CHECK_AND_RETURN_ERRORS_ASM(GetFillArgJump(assembler, &current_command, assembler->about_text.pointer_on_text[++i], number_of_compile));
+
+        if (number_of_compile == SECOND_COMPILE) {
+            FillListingFile(assembler->about_text.pointer_on_text[i], &current_command, listing_file, current_command.code_of_type_argument);
+        }
+
+        CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
     }
 
-    // if (number_of_compile == SECOND_COMPILE && assembler->byte_code_data.data[assembler->byte_code_data.size - 1] != CMD_HLT) { // FIXME empty lines
-    //     CHECK_AND_RETURN_ERRORS_ASM(ASM_EXPECTS_HLT);
-    // }
+    if (!find_cmd_hlt) {
+        CHECK_AND_RETURN_ERRORS_ASM(ASM_EXPECTS_HLT);
+    }
     
     PrintfByteCode(assembler, number_of_compile);
+
+    CLOSE_LISTING_FILE(listing_file);
 
     CHECK_AND_RETURN_ERRORS_ASM(AsmVerify(assembler, number_of_compile));
 
@@ -278,14 +308,15 @@ assembler_status Assemblirovanie(Assembler* assembler, int number_of_compile) {
 }
 
 assembler_status CreatByteCodeData(Assembler* assembler) {
-    assembler->byte_code_data.capacity = (size_t)assembler->cnt_commands;
-    assembler->byte_code_data.size     = 0;
+    assert(assembler);
 
-    assembler->byte_code_data.data = (type_t*)calloc(assembler->byte_code_data.capacity, sizeof(type_t));
+    assembler->byte_code_data.data = (type_t*)calloc(assembler->byte_code_data.size, sizeof(type_t));
 
     if (assembler->byte_code_data.data == NULL){
         CHECK_AND_RETURN_ERRORS_ASM(ASM_NOT_ENOUGH_MEMORY);
     }
+
+    assembler->byte_code_data.size = 0;
 
     return ASM_SUCCESS;
 }
--- asm/asm.h
+++ asm/asm.h
@@ -7,26 +7,10 @@
 
 #define CHECK_AND_RETURN_ERRORS_ASM(error, ...)        \
         if ((error) != ASM_SUCCESS) {                  \
-            fprintf(stderr, "Error is: %d\n", error);  \
+            fprintf(stderr, "Error is: %d, line %d\n", error, __LINE__);\
             __VA_ARGS__;                               \
             return (error);                            \
         }
-
-// TODO function
-#define CHECK_LABEL(string)                                                     \
-    if (*string == ':') {                                                       \
-        int number = 0;                                                         \
-                                                                                \
-        if (sscanf(string, "%*c%d", &number) == 1) {                            \
-            assembler->labels[number] = (number_of_compile == FIRST_COMPILE) ?  \
-                                        (type_t)assembler->cnt_commands : (type_t)assembler->byte_code_data.size; \
-            break;                                                              \
-        }                                                                       \
-                                                                                \
-        else {                                                                  \
-            return ASM_NOT_FOUND_LABEL;                                         \
-        }                                                                       \
-    }
 
 
 const int LEN_NAME_REGISTER = 3;
@@ -64,7 +48,6 @@ enum status_cmp {
 struct Byte_code_data {
     type_t* data;
     size_t size;
-    size_t capacity;
 };
 
 enum type_arguments {
@@ -79,20 +62,43 @@ struct About_commands {
     const char* command_name;
     type_t command_code;
     int code_of_type_argument;
+    type_t argument;
 };
 
 struct Assembler {
     Byte_code_data byte_code_data;
     About_text about_text;
     type_t labels[CNT_LABELS];
-    About_commands about_commands[MAX_CNT_COMMANDS];
-    int cnt_commands;
-    const char* name_registers[CNT_REGISTERS]     = {"RAX", "RBX", "RCX", "RDX", "REX", "RFX", "RGX", "RHX"};
-    const char* name_ram_registers[CNT_REGISTERS] = {"[RAX]", "[RBX]", "[RCX]", "[RDX]", "[REX]", "[RFX]", "[RGX]", "[RHX]"};
+    const char* const name_registers[CNT_REGISTERS] = {"RAX", "RBX", "RCX", "RDX", "REX", "RFX", "RGX", "RHX"};
 };
 
+const About_commands about_commands [] = {
+    {.command_name = "PUSH",  .command_code = CMD_PUSH,  .code_of_type_argument = NUM_ARGUMENT,     .argument = 0},
+    {.command_name = "POP",   .command_code = CMD_POP,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "ADD",   .command_code = CMD_ADD,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "SUB",   .command_code = CMD_SUB,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "DIV",   .command_code = CMD_DIV,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "MUL",   .command_code = CMD_MUL,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "SQRT",  .command_code = CMD_SQRT,  .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "POW",   .command_code = CMD_POW,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "IN",    .command_code = CMD_IN,    .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "OUT",   .command_code = CMD_OUT,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "HLT",   .command_code = CMD_HLT,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "JMP",   .command_code = CMD_JMP,   .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JB",    .command_code = CMD_JB,    .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JBE",   .command_code = CMD_JBE,   .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JA",    .command_code = CMD_JA,    .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JAE",   .command_code = CMD_JAE,   .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JE",    .command_code = CMD_JE,    .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "JNE",   .command_code = CMD_JNE,   .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "CALL",  .command_code = CMD_CALL,  .code_of_type_argument = LABEL_ARGUMENT,   .argument = 0},
+    {.command_name = "RET",   .command_code = CMD_RET,   .code_of_type_argument = NO_ARGUMENT,      .argument = 0},
+    {.command_name = "PUSHR", .command_code = CMD_PUSHR, .code_of_type_argument = REG_ARGUMENT,     .argument = 0},
+    {.command_name = "POPR",  .command_code = CMD_POPR,  .code_of_type_argument = REG_ARGUMENT,     .argument = 0},
+    {.command_name = "PUSHM", .command_code = CMD_PUSHM, .code_of_type_argument = RAM_REG_ARGUMENT, .argument = 0},
+    {.command_name = "POPM",  .command_code = CMD_POPM,  .code_of_type_argument = RAM_REG_ARGUMENT, .argument = 0}
+};
 
-void AsmInitInfoAboutCommands(Assembler* assembler);
 
 void AsmInitLabels(Assembler* assembler);
 
@@ -100,17 +106,23 @@ assembler_status AsmCtor(Assembler* assembler, const char* name_comands_file);
 
 assembler_status AsmVerify(const Assembler* assembler, int number_of_compile);
 
-status_cmp FillCommand(Assembler* assembler, char* command, int index, int number_of_compile);
+status_cmp FindCommand(Assembler* assembler, char* string, About_commands* current_command);
 
-assembler_status GetFillArgNum(Assembler* assembler, char* string, int number_of_compile);
+void FillCommand(Assembler* assembler, About_commands* current_command, int number_of_compile);
+
+assembler_status GetFillArgNum(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile);
 
 status_cmp CheckRegister(Assembler* assembler, char* string, int type_argument);
 
-assembler_status GetFillArgReg(Assembler* assembler, char* string, int number_of_compile, int type_argument);
+assembler_status GetFillArgReg(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile, int type_argument);
 
-assembler_status GetFillArgJump(Assembler* assembler, char* string, int number_of_compile);
+assembler_status GetFillArgJump(Assembler* assembler, About_commands* current_command, char* string, int number_of_compile);
+
+assembler_status CheckLabel(Assembler* assembler, char* string);
 
 assembler_status PrintfByteCode(Assembler* assembler, int number_of_compile);
+
+void FillListingFile(char* pointer_on_command, About_commands* current_command, FILE* listing_file, int type_argument);
 
 assembler_status Assemblirovanie(Assembler* assembler, int number_of_compile);
 
